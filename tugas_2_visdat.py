1b94cbfc9f764a43d9d219f99077aa21a1591500
Zim <83820580+zim18@users.noreply.github.com>
2023-03-25 22:51:08 +0700

    Add files via upload

---
+++ tugas_2_visdat.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+#read data
+df = pd.read_excel(r"C:\Users\Hazim\Documents\streamlit\tugas2\factbook.xlsx")
+
+#Bubble Chart
+#variable
+x_col = "  GDP per capita "
+y_col = " Life expectancy at birth"
+size_col = "  Population "
+clr = "Continent"
+hvr_name_col = "Country"
+
+fig = px.scatter(df, x=x_col, y=y_col, size=size_col, color=clr, hover_name=hvr_name_col,
+                 log_x=True, size_max=60)
+
+fig.update_layout(
+    title="GDP per capita vs Life expectancy",
+    xaxis_title="GDP per capita",
+    yaxis_title="Life expectancy"
+)
+st.plotly_chart(fig, height=1000, width=1200)
+
+#dropdown list
+select_col = st.selectbox("List Kolom", df.columns)
+
+st.write(df[select_col])
